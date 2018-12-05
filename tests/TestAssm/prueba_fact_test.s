deb142d3a630a51f75a56d4565ea2f3c1d7cb11b
aivaldi94 <angie.ivaldi@gmail.com>
2018-12-05 16:55:30 -0300

    intento de resolver test23a

--- tests/TestAssm/prueba_fact_test.s
+++ tests/TestAssm/prueba_fact_test.s
@@ -19,32 +19,31 @@ L9:
 .globl L0
 .type L0,@function
 L0:
-	popq %rcx
-	pushq %rcx
-	movq %rsp, %rcx
+	popq %rax
+	pushq %rax
+#	cmpq $0, %rsi
+	#jne nueva
+	#jmp *%rax
+
+	#nueva:
 	pushq %rbp
 	movq %rsp, %rbp
 	subq $1024, %rsp
 
-
 	L16:
 
-	movq (%rcx), %rdx
 	movq %rbp,  -32(%rbp)
 	movq $0, %r8
-	movq (%rcx), %rdx
-	
+
 	movq %r8,  -40(%rbp)
 	movq  -40(%rbp), %r9
 	movq  -32(%rbp), %r8
 	addq %r9, %r8
-	movq (%rcx), %rdx
-	
+
 	movq %r8,  -32(%rbp)
 	movq  -32(%rbp), %r8
 	movq %rdi, (%r8)
-	movq (%rcx), %rdx
-	
+
 	movq %rsi,  -8(%rbp)
 	movq $0, %r8
 
@@ -52,63 +51,59 @@ L0:
 	movq  -48(%rbp), %r9
 	movq  -8(%rbp), %r8
 	cmpq %r9, %r8
-	movq (%rcx), %rdx
+
 	je L3
 
 	L4:
 
 	movq %rbp,  -56(%rbp)
 	movq $0, %r8
-	movq (%rcx), %rdx
-	
+
 	movq %r8,  -64(%rbp)
 	movq  -64(%rbp), %r9
 	movq  -56(%rbp), %r8
 	addq %r9, %r8
-	movq (%rcx), %rdx
-	
+
 	movq %r8,  -56(%rbp)
 	movq  -56(%rbp), %r8
 	movq (%r8), %rdi
-	movq (%rcx), %rdx
 
 	movq  -8(%rbp), %r8
 	movq %r8, %rsi
-	movq (%rcx), %rdx
 
 	movq $1, %r8
 
 	movq %r8,  -16(%rbp)
 	movq  -16(%rbp), %r8
 	subq %r8, %rsi
-	movq (%rcx), %rdx
-	
+
 	call L0
 
-	movq (%rcx), %rdx
 	movq %rax, %r8
 
 	movq %r8,  -24(%rbp)
 	L5:
 
-	movq (%rcx), %rdx
 	movq  -24(%rbp), %rax
 	jmp L15
 
 	L3:
 
 	movq $1, %r8
-	movq (%rcx), %rdx
+
 	movq %r8,  -24(%rbp)
 	jmp L5
 
 	L15:
 
+
+
+
 	movq %rbp, %rsp
 	popq %rbp
-	movq (%rsp), %rdx
+	popq %rax
+	pushq %rax	
 	ret
-	
 .globl _tigermain
 .type _tigermain,@function
 _tigermain:
